63cfabe2ed3c33d001b952fbcf65c5f5a33a8b3b
patjawat <patjawat@gmail.com>
2022-09-16 15:02:05 +0700

    updarw

--- config/web.php
+++ config/web.php
@@ -67,12 +67,12 @@ $config = [
             ],
         ],
         'db' => $db,
-        'urlManager' => [
-            'enablePrettyUrl' => true,
-            'showScriptName' => false,
-            'rules' => [
-            ],
-        ],
+        // 'urlManager' => [
+        //     'enablePrettyUrl' => true,
+        //     'showScriptName' => false,
+        //     'rules' => [
+        //     ],
+        // ],
 
         'assetManager' => [
 			'bundles' => [
