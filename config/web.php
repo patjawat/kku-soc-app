0b4b43211f43e0e0f21d0839d959f5fbca648b2d
patjawat <patjawat@gmail.com>
2022-09-21 15:13:25 +0700

    update

--- config/web.php
+++ config/web.php
@@ -107,7 +107,7 @@ $config = [
     'as access' => [
         'class' => 'mdm\admin\components\AccessControl',
         'allowActions' => [
-            '*',
+            // '*',
             'special/*',
             'soc/events/user-request',
             'soc/events/save-image',
