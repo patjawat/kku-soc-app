f3286f1afb6d193830489fc1ab5bb3496051acaa
patjawat <patjawat@gmail.com>
2022-10-05 09:15:19 +0700

    update

--- views/category-type/view.php
+++ views/category-type/view.php
@@ -54,9 +54,6 @@ $this->params['breadcrumbs'][] = $this->title;
     'filterModel' => $searchModel,
     'columns' => [
         ['class' => 'yii\grid\SerialColumn'],
-
-        'id',
-        'category_type',
         'name',
         'title',
         [
