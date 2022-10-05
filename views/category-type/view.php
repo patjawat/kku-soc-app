fe2277876729848741baf0053a1b49cc62b2f3cb
patjawat <patjawat@gmail.com>
2022-10-05 09:15:51 +0700

    update

--- views/category-type/view.php
+++ views/category-type/view.php
@@ -22,8 +22,8 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <p>
        
-        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
+        <?php Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+        <?php Html::a('Delete', ['delete', 'id' => $model->id], [
             'class' => 'btn btn-danger',
             'data' => [
                 'confirm' => 'Are you sure you want to delete this item?',
