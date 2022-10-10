bde9c8f5d4c61b283d1b472aa353f4575c8ebffa
patjawat <patjawat@gmail.com>
2022-10-10 14:56:31 +0700

    ียกฟะำ

--- modules/line/views/events/index.php
+++ modules/line/views/events/index.php
@@ -1,55 +1,132 @@
 <?php
-use yii\web\View;
-use yii\helpers\Html;
 use kartik\detail\DetailView;
+use yii\helpers\Html;
+use yii\web\View;
 $title = "ยังไม่รับเรื่อง";
 /** @var yii\web\View $this */
 ?>
 
 <h1 data-aos="fade-down" class="text-center">ยังไม่รับเรื่อง</h1>
-<?php $i =  3; ?>
-<?php foreach ($models as $model):?>
-<div class="card card-widget collapsed-card" data-aos="fade-up" data-aos-delay="<?=($i++)*100?>">
+<?php $i = 3;?>
+<?php foreach ($models as $model): ?>
+
+
+<div class="card card-primary collapsed-card" data-aos="fade-up" data-aos-delay="<?=($i++) * 100?>">
     <div class="card-header">
         <div class="user-block">
-<?=Html::img('@web/img/check_item.png',['class' => 'img-circle','data-cfsrc' => '@web/img/check_item.png']);?>
+            <?=Html::img('@web/img/check_item.png', ['class' => 'img-circle', 'data-cfsrc' => '@web/img/check_item.png']);?>
             <span class="username"><?=$model->eventType->name;?></a></span>
-            <span class="description">เวลาเกิดเหตุ <?=$model->event_date;?></span>
+            <span class="description text-light bg-dark">เวลาเกิดเหตุ <?=$model->event_date;?></span>
         </div>
-
         <div class="card-tools">
-            <button type="button" class="btn btn-tool" data-card-widget="collapse">
-                <i class="fas fa-plus"></i>
+            <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-plus"></i>
             </button>
-       
         </div>
+        
 
     </div>
 
     <div class="card-body" style="display: none;">
-        <button type="button" class="btn btn-default btn-sm"><i class="fas fa-share"></i> Share</button>
-        <button type="button" class="btn btn-default btn-sm"><i class="far fa-thumbs-up"></i> Like</button>
-        <span class="float-right text-muted">45 likes - 2 comments</span>
-        <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'reporter',
-            'result',
-            'backup_to',
-            'note',
+    <?php echo DetailView::widget([
+    'model' => $model,
+    'responsive' => true,
+    'attributes' => [
+        [
+            'columns' => [
+                [
+                    'attribute' => 'fname',
+                    'displayOnly' => true,
+                    'valueColOptions' => ['style' => 'width:30%'],
+                    'value' => $model->fname . ' ' . $model->lname,
+                ],
+                [
+                    'attribute' => 'person_type',
+                    'format' => 'raw',
+                    'valueColOptions' => ['style' => 'width:30%'],
+                    'displayOnly' => true,
+                    'type' => DetailView::INPUT_TEXT,
+                    'value' => $model->personType->name,
+                ],
+            ],
         ],
-    ]) ?>
-    </div>
-    <div class="card-footer" style="display: none;">
-       
+        [
+            'columns' => [
+                [
+                    'attribute' => 'phone',
+                    'displayOnly' => true,
+                    'valueColOptions' => ['style' => 'width:30%'],
+                ],
+                [
+                    'attribute' => 'event_type',
+                    'format' => 'raw',
+                    'valueColOptions' => ['style' => 'width:30%'],
+                    'displayOnly' => true,
+                    'value' => $model->eventType->name,
+                ],
+            ],
+        ],
+        [
+            'columns' => [
+                [
+                    'attribute' => 'fname',
+                    'displayOnly' => true,
+                    'valueColOptions' => ['style' => 'width:30%'],
+                    'format' => 'raw',
+                    'value' => $model->fname . ' ' . $model->lname,
+                ],
+                [
+                    'attribute' => 'event_date',
+                    'displayOnly' => true,
+                    'format' => 'raw',
+                    'type' => DetailView::INPUT_DATE,
+                    'widgetOptions' => [
+                        'pluginOptions' => ['format' => 'yyyy-mm-dd'],
+                    ],
+                ],
+            ],
+        ],
+        [
+            'columns' => [
+
+                [
+                    'attribute' => 'orther',
+                    'displayOnly' => true,
+                    'valueColOptions' => ['style' => 'width:30%'],
+                    'format' => 'raw',
+                    // 'label' => 'Provincia Nascita'
+                ],
+                [
+                    'attribute' => 'orther',
+                    'displayOnly' => true,
+                    'valueColOptions' => ['style' => 'width:30%'],
+                    'format' => 'raw',
+                    // 'label' => 'Provincia Nascita'
+                ],
+            ],
+        ],
+        [
+            'columns' => [
+                [
+                    'attribute' => 'orther',
+                    'displayOnly' => true,
+                    'format' => 'raw',
+                ],
+            ],
+        ],
+
+        
+    ],
+]);
+?>
     </div>
 
 </div>
+
 <?php endforeach;?>
 
 
 <!-- <img id="pictureUrl"> -->
-  <!-- <button id="btnLogIn" onclick="logIn()">Log In</button>
+<!-- <button id="btnLogIn" onclick="logIn()">Log In</button>
   <button id="btnLogOut" onclick="logOut()">Log Out</button> -->
 
 <?php
@@ -86,8 +163,8 @@ function logOut() {
       //   }
       // }
     }
-    main()
-    
+    // main()
+
 JS;
-$this->registerJS($js,View::POS_END)
+$this->registerJS($js, View::POS_END)
 ?>
