5cc5ce94215be660bab9efcd65b531a77a59d102
patjawat <patjawat@gmail.com>
2022-10-11 10:24:04 +0700

    udpate

--- modules/soc/views/events/view.php
+++ modules/soc/views/events/view.php
@@ -299,7 +299,7 @@ $this->params['breadcrumbs'][] = $this->title;
                   <div class="d-flex justify-content-between align-items-center">
                     <div class="btn-group">
                       <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                      <!-- <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button> -->
                     </div>
                     <small class="text-muted">9 mins</small>
                   </div>
