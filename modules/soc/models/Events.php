43cb8e2632dd8fcff5ce4ce25fb8a693cfc2f292
patjawat <patjawat@gmail.com>
2022-10-11 09:57:01 +0700

    update

--- modules/soc/models/Events.php
+++ modules/soc/models/Events.php
@@ -44,8 +44,8 @@ class Events extends \yii\db\ActiveRecord
             [['data_json'], 'string'],
             [['fname', 'lname', 'person_type', 'phone', 'event_date', 'accept_pdpa','department','event_type','orther'], 'required'],
             [['person_type', 'event_type', 'result', 'backup_type', 'reporter', 'created_by', 'updated_by'], 'integer'],
-            [['event_date', 'updated_at', 'created_at','event_group','files','worker'], 'safe'],
-            [['ref', 'fname', 'lname', 'fullname', 'department', 'address', 'phone', 'orther', 'event_location_note', 'lat', 'lng', 'work_img', 'docs', 'note'], 'string', 'max' => 255],
+            [['event_date', 'updated_at', 'created_at','event_group','files','worker', 'note'], 'safe'],
+            [['ref', 'fname', 'lname', 'fullname', 'department', 'address', 'phone', 'orther', 'event_location_note', 'lat', 'lng', 'work_img', 'docs'], 'string', 'max' => 255],
             [['accept_pdpa'], 'string', 'max' => 1],
         ];
     }
