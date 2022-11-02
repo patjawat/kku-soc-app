15d39a419abb3798e4a20a24e4fb9208460f1aeb
patjawat <patjawat@gmail.com>
2022-11-02 11:22:05 +0700

    update

--- modules/soc/controllers/DefaultController.php
+++ modules/soc/controllers/DefaultController.php
@@ -3,10 +3,8 @@
 namespace app\modules\soc\controllers;
 
 use app\modules\soc\models\Events;
-use yii\base\Response;
 use yii\helpers\BaseFileHelper;
 use yii\web\Controller;
-use app\models\Uploads;
 use app\components\SystemHelper;
 
 /**
@@ -26,42 +24,67 @@ class DefaultController extends Controller
     public function actionZip($ref)
     {
 
-        // $zip_file_name_with_location = "/zip-file/all-image.zip";
-        // touch($zip_file_name_with_location); // just create a zip file
         \Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
-
-        $this->CreateDir($ref);
+        // $this->CreateDir($ref);
         $basePath = SystemHelper::getUploadPath().$ref;
-        $zip = new \ZipArchive();
-        $opening_zip = $zip->open($basePath);
- 
-        $files = Uploads::find()->where(['ref' =>$ref])->all(); //
-       
-        $datas= [];
-        $zipname = "example_zip"; // give zip file name
-        $zip->open($zipname, \ZipArchive::CREATE); 
+        // $zip = new \ZipArchive();
+        // $opening_zip = $zip->open($basePath);
 
-        foreach ($files as $key => $file) {
-            $filename = $basePath.$ref.'/'.$file->real_filename;
-                    if(file_exists($filename)) {
-                       $zip->addFile($filename); 
-                    }
-                }
-                $zip->close();
-    //  return $files;
-            // $zip = new ZipArchive(); //Create an object of ZipArchive class
-            
-        // $zipname = "example_zip"; // give zip file name
-        // $zip->open($zipname, ZipArchive::CREATE); //example_zip zip file created
+        // $files = Uploads::find()->where(['ref' =>$ref])->all(); //
+
+        // $datas= [];
+        // $destination="@web/download/filename.zip";
+        // $zip->open($destination, \ZipArchive::CREATE);
 
         // foreach ($files as $key => $file) {
-        //     $zip->addFile($file); //add each file into example_zip zip file
-        // }
+        //     $filename = $basePath.$ref.'/'.$file->real_filename;
+        //             if(file_exists($filename)) {
+        //                $zip->addFile($filename);
+        //             }
+        //         }
+        // $zip->close();
 
-        // $zip->close(); // zip file with files created successful now close it
-        // return $files;
+        $zipArchive = new \ZipArchive();
+        $zipFile = "@web/download/example-zip-file.zip";
+        if ($zipArchive->open($zipFile, \ZipArchive::CREATE) !== true) {
+            exit("Unable to open file.");
+        }
+        // $folder = 'example-folder/';
+        $folder = $basePath;
+        $this->createZip($zipArchive, $folder);
+        $zipArchive->close();
+        return 'Zip file created.';
 
     }
+
+
+    private function createZip($zipArchive, $folder)
+{
+    if (is_dir($folder)) {
+        if ($f = opendir($folder)) {
+            while (($file = readdir($f)) !== false) {
+                if (is_file($folder . $file)) {
+                    if ($file != '' && $file != '.' && $file != '..') {
+                        $zipArchive->addFile($folder . $file);
+                    }
+                } else {
+                    if (is_dir($folder . $file)) {
+                        if ($file != '' && $file != '.' && $file != '..') {
+                            $zipArchive->addEmptyDir($folder . $file);
+                            $folder = $folder . $file . '/';
+                            createZip($zipArchive, $folder);
+                        }
+                    }
+                }
+            }
+            closedir($f);
+        } else {
+            exit("Unable to open directory " . $folder);
+        }
+    } else {
+        exit($folder . " is not a directory.");
+    }
+}
 
     private function CreateDir($folderName)
     {
