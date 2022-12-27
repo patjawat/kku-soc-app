0eae91549c7438b87275f9f287cab2a91ce0bc93
patjawat <patjawat@gmail.com>
2022-12-27 11:15:12 +0700

    u

--- modules/soc/views/report/index.php
+++ modules/soc/views/report/index.php
@@ -6,48 +6,32 @@ use yii\helpers\Html;
 <div class="row">
     <div class="col-3">
         <?php echo $this->render('_search', ['model' => $searchModel]); ?>
+        <div class="nav flex-column nav-tabs h-100" id="vert-tabs-tab" role="tablist" aria-orientation="vertical">
+<a class="nav-link active" id="vert-tabs-home-tab" data-toggle="pill" href="#vert-tabs-home" role="tab" aria-controls="vert-tabs-home" aria-selected="true">รายงานบบที่ 1</a>
+<a class="nav-link" id="vert-tabs-profile-tab" data-toggle="pill" href="#vert-tabs-profile" role="tab" aria-controls="vert-tabs-profile" aria-selected="false">รายงานบบที่ 2 มีภาพประกอบ</a>
+</div>
     </div>
     <div class="col-9">
-
-        <ul class="nav nav-tabs mt-4" id="custom-content-below-tab" role="tablist">
-            <li class="nav-item">
-                <a class="nav-link active" id="custom-content-below-home-tab" data-toggle="pill"
-                    href="#custom-content-below-home" role="tab" aria-controls="custom-content-below-home"
-                    aria-selected="true">รายงานแบบที่ 1</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" id="custom-content-below-profile-tab" data-toggle="pill"
-                    href="#custom-content-below-profile" role="tab" aria-controls="custom-content-below-profile"
-                    aria-selected="false">รายงานแบบที่ 2 มีภาพประกอบ</a>
-            </li>
-
-        </ul>
-
-        <?php if ($searchModel->q_date): ?>
-        <div class="tab-content" id="custom-content-below-tabContent">
-            <div class="tab-pane fade active show" id="custom-content-below-home" role="tabpanel"
-                aria-labelledby="custom-content-below-home-tab">
-
+    <?php if ($searchModel->q_date): ?>
+    <div class="tab-content" id="vert-tabs-tabContent">
+<div class="tab-pane text-left fade active show" id="vert-tabs-home" role="tabpanel" aria-labelledby="vert-tabs-home-tab">
                 <h1 id="loading" class="text-center mt-5"> <i class="fas fa-circle-notch fa-spin"></i> กำลังโหลด...</h1>
                 <div id="view-frame">
-                    <?php  '<p>' . Html::a('ดาวน์โหลดเอกสาร', Url::to(Yii::getAlias('@web') . '/msword/ms_word_result.docx'), ['class' => 'btn btn-info']) .
-        '</p><iframe src="https://docs.google.com/viewerng/viewer?url=' . Url::to(Yii::getAlias('@web') . '/msword/ms_word_result.docx', true) . '&embedded=true"  style="position: absolute;width:100%; height: 100%;border: none;"></iframe>';?>
+                    <?php echo '<p>' . Html::a('ดาวน์โหลดเอกสาร', Url::to(Yii::getAlias('@web') . '/msword/ms_word_result.docx'), ['class' => 'btn btn-info']) .
+        '</p><iframe src="https://docs.google.com/viewerng/viewer?url=' . Url::to(Yii::getAlias('@web') . '/msword/ms_word_result.docx', true) . '&embedded=true"  style="position: absolute;width:100%; height:500px;border: none;"></iframe>';?>
                 </div>
 
-            </div>
-            <div class="tab-pane fade" id="custom-content-below-profile" role="tabpanel"
-                aria-labelledby="custom-content-below-profile-tab">
+</div>
+<div class="tab-pane fade" id="vert-tabs-profile" role="tabpanel" aria-labelledby="vert-tabs-profile-tab">
+<?php echo '<p>' . Html::a('ดาวน์โหลดเอกสาร', Url::to(Yii::getAlias('@web') . '/msword/ms_word_result2.docx'), ['class' => 'btn btn-info']) .
+        '</p><iframe src="https://docs.google.com/viewerng/viewer?url=' . Url::to(Yii::getAlias('@web') . '/msword/ms_word_result2.docx', true) . '&embedded=true"  style="position: absolute;width:100%; height:500px;border: none;"></iframe>';?>
 
-                <?php  '<p>' . Html::a('ดาวน์โหลดเอกสาร', Url::to(Yii::getAlias('@web') . '/msword/ms_word_result2.docx'), ['class' => 'btn btn-info']) .
-        '</p><iframe src="https://docs.google.com/viewerng/viewer?url=' . Url::to(Yii::getAlias('@web') . '/msword/ms_word_result2.docx', true) . '&embedded=true"  style="position: absolute;width:100%; height: 100%;border: none;"></iframe>';?>
+</div>
 
-            </div>
+</div>
+<?php endif; ?>
 
-
-        </div>
-
-        <?php endif; ?>
-
+ 
 
     </div>
 </div>
