60fe6f689daacd9189490e63bcccf97185859858
patjawat <patjawat@gmail.com>
2022-12-27 10:45:12 +0700

    y

--- modules/soc/views/report/index.php
+++ modules/soc/views/report/index.php
@@ -24,7 +24,7 @@ use yii\helpers\Url;
 
 <h1 id="loading" class="text-center mt-5"> <i class="fas fa-circle-notch fa-spin"></i> กำลังโหลด...</h1>
         <div id="view-frame">
-            <?php  echo '<iframe src="https://docs.google.com/viewerng/viewer?url=' . Url::to(Yii::getAlias('@web') . '/msword/ms_word_result.docx', true) . '&embedded=true"  style="width:98%; height:800px;border: none;"></iframe>';?>
+            <?php  echo '<iframe src="https://docs.google.com/viewerng/viewer?url=' . Url::to(Yii::getAlias('@web') . '/msword/ms_word_result2.docx', true) . '&embedded=true"  style="width:98%; height:800px;border: none;"></iframe>';?>
         </div>
 
 </div>
