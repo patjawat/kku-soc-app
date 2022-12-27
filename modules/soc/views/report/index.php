b24619194773c4b4d440aa952c4ecc3c0a2bf7b1
patjawat <patjawat@gmail.com>
2022-12-27 10:19:04 +0700

    ะ

--- modules/soc/views/report/index.php
+++ modules/soc/views/report/index.php
@@ -1,11 +1,10 @@
 <?php
 use yii\helpers\Url;
 ?>
+<h1>ระบบรายงานประจำเดือน</h1>
 <div class="row">
     <div class="col-3">
-
         <?php echo $this->render('_search', ['model' => $searchModel]); ?>
-
     </div>
     <div class="col-9">
         <?php if ($searchModel->q_date): ?>
