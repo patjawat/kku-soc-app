14b5e8fcf20ce7fb6551ce326cbf995f5c606c34
patjawat <patjawat@gmail.com>
2022-12-27 10:48:44 +0700

    u

--- modules/soc/views/report/index.php
+++ modules/soc/views/report/index.php
@@ -7,39 +7,45 @@ use yii\helpers\Url;
         <?php echo $this->render('_search', ['model' => $searchModel]); ?>
     </div>
     <div class="col-9">
-        
-    <ul class="nav nav-tabs" id="custom-content-below-tab" role="tablist">
-<li class="nav-item">
-<a class="nav-link active" id="custom-content-below-home-tab" data-toggle="pill" href="#custom-content-below-home" role="tab" aria-controls="custom-content-below-home" aria-selected="true">Home</a>
-</li>
-<li class="nav-item">
-<a class="nav-link" id="custom-content-below-profile-tab" data-toggle="pill" href="#custom-content-below-profile" role="tab" aria-controls="custom-content-below-profile" aria-selected="false">Profile</a>
-</li>
 
-</ul>
+        <ul class="nav nav-tabs" id="custom-content-below-tab" role="tablist">
+            <li class="nav-item">
+                <a class="nav-link active" id="custom-content-below-home-tab" data-toggle="pill"
+                    href="#custom-content-below-home" role="tab" aria-controls="custom-content-below-home"
+                    aria-selected="true">รายงานแบบที่ 1</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" id="custom-content-below-profile-tab" data-toggle="pill"
+                    href="#custom-content-below-profile" role="tab" aria-controls="custom-content-below-profile"
+                    aria-selected="false">รายงานแบบที่ 2 มีภาพประกอบ</a>
+            </li>
 
-<?php if ($searchModel->q_date): ?>
-<div class="tab-content" id="custom-content-below-tabContent">
-<div class="tab-pane fade active show" id="custom-content-below-home" role="tabpanel" aria-labelledby="custom-content-below-home-tab">
+        </ul>
 
-<h1 id="loading" class="text-center mt-5"> <i class="fas fa-circle-notch fa-spin"></i> กำลังโหลด...</h1>
-        <div id="view-frame">
-            <?php  echo '<iframe src="https://docs.google.com/viewerng/viewer?url=' . Url::to(Yii::getAlias('@web') . '/msword/ms_word_result2.docx', true) . '&embedded=true"  style="width:98%; height:800px;border: none;"></iframe>';?>
+        <?php if ($searchModel->q_date): ?>
+        <div class="tab-content" id="custom-content-below-tabContent">
+            <div class="tab-pane fade active show" id="custom-content-below-home" role="tabpanel"
+                aria-labelledby="custom-content-below-home-tab">
+
+                <h1 id="loading" class="text-center mt-5"> <i class="fas fa-circle-notch fa-spin"></i> กำลังโหลด...</h1>
+                <div id="view-frame">
+                    <?php  echo '<iframe src="https://docs.google.com/viewerng/viewer?url=' . Url::to(Yii::getAlias('@web') . '/msword/ms_word_result.docx', true) . '&embedded=true"  style="width:98%; height:800px;border: none;"></iframe>';?>
+                </div>
+
+            </div>
+            <div class="tab-pane fade" id="custom-content-below-profile" role="tabpanel"
+                aria-labelledby="custom-content-below-profile-tab">
+
+                <?php  echo '<iframe src="https://docs.google.com/viewerng/viewer?url=' . Url::to(Yii::getAlias('@web') . '/msword/ms_word_result2.docx', true) . '&embedded=true"  style="width:98%; height:800px;border: none;"></iframe>';?>
+
+            </div>
+
+
         </div>
 
-</div>
-<div class="tab-pane fade" id="custom-content-below-profile" role="tabpanel" aria-labelledby="custom-content-below-profile-tab">
-
-<?php  echo '<iframe src="https://docs.google.com/viewerng/viewer?url=' . Url::to(Yii::getAlias('@web') . '/msword/ms_word_result2.docx', true) . '&embedded=true"  style="width:98%; height:800px;border: none;"></iframe>';?>
-
-</div>
+        <?php endif; ?>
 
 
-</div>
-
-<?php endif; ?>
-        
-       
     </div>
 </div>
 
