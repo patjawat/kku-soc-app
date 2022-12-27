8c8568d6c8f8c51315e987b51f667e05011e49ea
patjawat <patjawat@gmail.com>
2022-12-27 10:53:26 +0700

    u

--- modules/soc/views/report/index.php
+++ modules/soc/views/report/index.php
@@ -8,7 +8,7 @@ use yii\helpers\Url;
     </div>
     <div class="col-9">
 
-        <ul class="nav nav-tabs" id="custom-content-below-tab" role="tablist">
+        <ul class="nav nav-tabs mt-4" id="custom-content-below-tab" role="tablist">
             <li class="nav-item">
                 <a class="nav-link active" id="custom-content-below-home-tab" data-toggle="pill"
                     href="#custom-content-below-home" role="tab" aria-controls="custom-content-below-home"
