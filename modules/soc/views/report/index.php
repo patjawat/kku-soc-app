ca1abf55ad83d2dab688b73f11a869486c801a5f
patjawat <patjawat@gmail.com>
2022-12-27 10:11:35 +0700

    ี

--- modules/soc/views/report/index.php
+++ modules/soc/views/report/index.php
@@ -2,20 +2,36 @@
 use yii\helpers\Url;
 ?>
 <div class="row">
-<div class="col-3">
+    <div class="col-3">
 
-<?php echo $this->render('_search', ['model' => $searchModel]); ?>
+        <?php echo $this->render('_search', ['model' => $searchModel]); ?>
 
+    </div>
+    <div class="col-9">
+        <?php if ($searchModel->q_date): ?>
+        <h1 id="loading" class="text-center">กำลังโหลด...</h1>
+        <div id="view-frame">
+            <?php  echo '<iframe src="https://docs.google.com/viewerng/viewer?url=' . Url::to(Yii::getAlias('@web') . '/msword/ms_word_result.docx', true) . '&embedded=true"  style="width:98%; height:800px;border: none;"></iframe>';?>
+            <?php endif; ?>
+        </div>
+    </div>
 </div>
-<div class="col-9">
-    
-
-<?php if ($searchModel->q_date): ?>
 
 <?php
-echo '<iframe src="https://docs.google.com/viewerng/viewer?url=' . Url::to(Yii::getAlias('@web') . '/msword/ms_word_result.docx', true) . '&embedded=true"  style="width:98%; height:800px;border: none;"></iframe>';
-    ?>
-    <?php endif; ?>
-</div>
-</div>
+$js = <<< JS
+// window.onbeforeunload = function () { 
+//    console.log("onbeforeunload");
+//  }
+$('#view-frame').hide()
+$('#loading').show();
+$(window).on('load', function() {
+    $('#loading').hide();
+console.log('loading');
+$('#view-frame').show()
+  });
  
+
+
+JS;
+$this->registerJs($js);
+?>
