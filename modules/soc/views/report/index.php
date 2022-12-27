2b866aabb180a85afce1db5fb36f4238a1fb40a0
patjawat <patjawat@gmail.com>
2022-12-27 10:14:59 +0700

    u

--- modules/soc/views/report/index.php
+++ modules/soc/views/report/index.php
@@ -9,7 +9,7 @@ use yii\helpers\Url;
     </div>
     <div class="col-9">
         <?php if ($searchModel->q_date): ?>
-        <h1 id="loading" class="text-center">กำลังโหลด...</h1>
+        <h1 id="loading" class="text-center mt-5"> <i class="fas fa-circle-notch fa-spin"></i> กำลังโหลด...</h1>
         <div id="view-frame">
             <?php  echo '<iframe src="https://docs.google.com/viewerng/viewer?url=' . Url::to(Yii::getAlias('@web') . '/msword/ms_word_result.docx', true) . '&embedded=true"  style="width:98%; height:800px;border: none;"></iframe>';?>
             <?php endif; ?>
