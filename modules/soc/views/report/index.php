6ffeaa30dc0c5bd4ed701396c9072dfb3c14246a
patjawat <patjawat@gmail.com>
2022-12-27 09:32:00 +0700

    u

--- modules/soc/views/report/index.php
+++ modules/soc/views/report/index.php
@@ -9,3 +9,4 @@ use yii\helpers\Url;
 echo '<iframe src="https://docs.google.com/viewerng/viewer?url=' . Url::to(Yii::getAlias('@web') . '/msword/ms_word_result.docx', true) . '&embedded=true"  style="position: absolute;width:92%; height: 100%;border: none;"></iframe>';
 ?>
     <?php endif; ?>
+    s
