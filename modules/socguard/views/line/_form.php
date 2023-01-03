0897605e5ccc0341ad7e92b158ca359407a1a328
patjawat <patjawat@gmail.com>
2023-01-03 16:07:31 +0700

    update

--- modules/socguard/views/line/_form.php
+++ modules/socguard/views/line/_form.php
@@ -8,17 +8,18 @@ use yii\widgets\ActiveForm;
 /** @var yii\widgets\ActiveForm $form */
 ?>
 
-<div class="borrow-form">
+<div class="row justify-content-center mt-3" id="warp-content">
+<div class="text-center" >
+  <?=Html::img('@web/images/join2.png',['width' => '400px'])?>
+</div>
 
     <?php $form = ActiveForm::begin(); ?>
 
-
-    <?= $form->field($model, 'item_code')->textInput(['maxlength' => true]) ?>
-
     <div class="form-group">
-        <?= Html::submitButton('ยืนยันการเบิก', ['class' => 'btn btn-success','id' => 'btn-save']) ?>
+        <?= Html::submitButton('ยืนยันการเบิก', ['class' => 'btn btn-lg btn-primary','id' => 'btn-save']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
 
 </div>
+<div>
